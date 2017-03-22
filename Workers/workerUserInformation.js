3d142f9902da16cfa3b39894463a9886c7e299b1
Sebastian Guevara <rlxsebas@gmail.com>
2017-03-22 14:48:28 -0500

    refactor: Updating workerUserInformation - adding csrf security token

--- Workers/workerUserInformation.js
+++ Workers/workerUserInformation.js
@@ -1,14 +1,11 @@
 self.addEventListener('message', function (e) {
-
     var data = e.data;
     var http = new XMLHttpRequest();
-    var url = "https://100l-app.teleows.com/servicecreator/pageservices/service.do?forAccessLog={serviceName:um_user_get,username:" + e.data.username + ",tenantId:" + e.data.tenantId + "}";
-    var params = "username="+e.data.username+"&serviceId=um_user_get";
+    var url = "https://100l-app.teleows.com/servicecreator/pageservices/service.do?forAccessLog={serviceName:um_user_get,userId:" + e.data.userId + ",tenantId:" + e.data.tenantId + "}";
+    var params = "user_id="+e.data.userId+"&serviceId=um_user_get&csrfToken="+e.data.token;
     http.open("POST", url, true);
-
     //Send the proper header information along with the request
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
     http.onreadystatechange = function () {//Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
             self.postMessage(http.response);
@@ -16,8 +13,6 @@ self.addEventListener('message', function (e) {
         }
     }
     http.send(params);
-    
-
 }, false);
 
 
