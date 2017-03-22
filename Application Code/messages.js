a6606bb1a58ab520cf427a1705ee8c6d9bd73750
Sebastian Guevara <rlxsebas@gmail.com>
2017-03-22 09:21:14 -0500

    refactor: Creating message Module

---
+++ Application Code/messages.js
@@ -0,0 +1,14 @@
+module.exports = {
+    addMessageLoder: function (selector,location) {
+        $(location).addClass("loader");
+        $(location).append("<div id='loader' class='loader-container text-center color-white'><div><i style='color:white' class='fa fa-spinner fa-pulse fa-3x'></i></div><div style='color:white'><h4>Smart Docs <br> <small> Cargando Recursos <div id='"+selector+"'> </div> </small> <br><small>... Se esta preparando para ti ...</small></h4><h5>Desarollado por: Huawei Colombia  <br> OSS IT Team </h5></div></div>");
+    },
+    changeMessageLoader: function (selector, msg) {
+        console.log("Selector: "+ selector);
+        console.log("Message: " + msg);
+        $("#" + selector).text(msg);
+    },
+    removeMessageLoader: function (selector){
+        $("#"+selector).remove();
+    }
+}
