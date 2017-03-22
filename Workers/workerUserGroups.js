37ca372f2da8020fb56cdae74c784d5efb8cdaa6
Sebastian Guevara <rlxsebas@gmail.com>
2017-03-22 17:59:17 -0500

    extra: Adding workerUserGroup JS Version

--- Workers/workerUserGroups.js
+++ Workers/workerUserGroups.js
@@ -2,8 +2,8 @@ self.addEventListener('message', function (e) {
 
     var data = e.data;
     var http = new XMLHttpRequest();
-    var url = "https://100l-app.teleows.com/servicecreator/pageservices/service.do?forAccessLog={serviceName:um_group_member_getList,username:" + e.data.username + ",tenantId:" + e.data.tenantId + "}";
-    var params = "start=0&limit=100&member_name="+e.data.username+"&serviceId=um_group_member_getList";
+    var url = "https://100l-app.teleows.com/servicecreator/pageservices/service.do?forAccessLog={serviceName:um_group_member_getList,userId:" + e.data.userId + ",tenantId:" + e.data.tenantId + "}";
+    var params = "start=0&limit=100&member_name="+e.data.username+"&csrfToken="+e.data.token+"&serviceId=um_group_member_getList";
     http.open("POST", url, true);
 
     //Send the proper header information along with the request
